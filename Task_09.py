db1e463e3301369cd32faf165903ecacedd57ceb
Elena <keaya@rambler.ru>
2023-06-08 13:56:25 +0300

    исправила вывод таблицы умножения

--- Task_09.py
+++ Task_09.py
@@ -7,5 +7,5 @@ MAX = 9
 END = 10
 for i in range(MIN, MAX + 1):
     for j in range(MIN, END + 1):
-        print(f'{i} x {j} = {i * j}')
+        print(f'{j} x {i} = {i * j}', end='\t')
     print(' ')
